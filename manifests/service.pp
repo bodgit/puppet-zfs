2d2b69e2880f21d7fbf9cd15c922bd10c0c50f72
Matt Dainty <matt@bodgit-n-scarper.com>
2021-06-24 11:32:35 +0100

    Guard the modprobe exec with $zfs::service_manage

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,16 +1,17 @@
 # @!visibility private
 class zfs::service {
 
-  # The ZFS SysV init scripts are all guarded with checking if the ZFS
-  # module is loaded and return 0 if it's not. However returning 0 for
-  # `service foo status` indicates it's running so Puppet will think the
-  # services are all running and therefore never try to start them
-  exec { 'modprobe zfs':
-    path   => $facts['path'],
-    unless => 'grep -q "^zfs " /proc/modules',
-  }
-
   if $zfs::service_manage {
+
+    # The ZFS SysV init scripts are all guarded with checking if the ZFS
+    # module is loaded and return 0 if it's not. However returning 0 for
+    # `service foo status` indicates it's running so Puppet will think the
+    # services are all running and therefore never try to start them
+    exec { 'modprobe zfs':
+      path   => $facts['path'],
+      unless => 'grep -q "^zfs " /proc/modules',
+    }
+
     case $facts['service_provider'] {
       'systemd': {
         $cache_ensure = str2bool($facts['zfs_zpool_cache_present']) ? {
